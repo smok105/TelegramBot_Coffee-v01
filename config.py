989b89f05562300f024c20ad38e36d02528bb47f
smok105 <63140604+smok105@users.noreply.github.com>
2020-04-16 22:20:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-token = '1249266036:AAHUEPR1XWWLHk1nI5C1ILwENSU_XK1wOeU'  # TOKEN take a tale bot bot father
+token = ''  # TOKEN take a tale bot bot father
 
-owner = '416430147' # ид человека который будет получать смс от бота, можно узнать через телеграм бот.393765227(
-# Вероника)416430147(ДимаАД)
+owner = '' # ид человека который будет получать смс от бота
